dd5dbadc2bdd1de0d16dc19b1873d45c9c2a422a
Plamen Tserov <plamen.tserov@donatix.net>
2020-05-01 11:37:59 +0300

    this update: -adds: --npm module - custom scroll bar --new images -creates: --horizontal scroll section --new styles

---
+++ src/components/HorizontalScrollSection.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <h2 class="section-title">{{heading}}</h2>
+        <vue-custom-scrollbar class="scroll-area container"
+                              :settings="settings" @ps-scroll-y="scrollHanle">
+                              <div class="d-flex py-2">
+                                <slot></slot>
+                              </div>
+        </vue-custom-scrollbar>
+    </div>
+</template>
+
+<script>
+import vueCustomScrollbar from 'vue-custom-scrollbar';
+
+export default {
+  props: ['heading'],
+  name: 'HorizontalScrollSection',
+  components: {
+    vueCustomScrollbar,
+  },
+  data() {
+    return {
+      settings: {
+        maxScrollbarLength: 450,
+      },
+    };
+  },
+  methods: {
+    scrollHanle(evt) {
+      console.log(evt);
+    },
+  },
+};
+</script>
+<style >
+.scroll-area {
+  position: relative;
+  margin: auto;
+  height: auto;
+}
+.ps__rail-x.ps--clicking .ps__thumb-x,
+.ps__rail-x:focus>.ps__thumb-x,
+.ps__rail-x:hover>.ps__thumb-x{
+    height: 6px !important;
+}
+.ps .ps__rail-x.ps--clicking,
+.ps .ps__rail-x:focus,
+.ps .ps__rail-x:hover,
+.ps .ps__rail-y.ps--clicking,
+.ps .ps__rail-y:focus, .ps .ps__rail-y:hover{
+    background: transparent !important;
+}
+</style>
